994bb165c2beb8a4e696979b6bf1ca2cf2c4fd0e
Jagan Teki <jagan@amarulasolutions.com>
2020-05-19 17:07:24 +0530

    roc-rk3399-pc: Add board detection via I2C2
    Signed-off-by: Suniel Mahesh <sunil@amarulasolutions.com>
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- board/firefly/roc-pc-rk3399/roc-pc-rk3399.c
+++ board/firefly/roc-pc-rk3399/roc-pc-rk3399.c
@@ -39,10 +39,18 @@ out:
 #define BUS_NUM				2
 #define ROC_RK3399_MEZZ_BAT_ADDR	0x62
 
+enum roc_rk3399_pc_board_type {
+	ROC_RK3399_PC,			/* roc-rk3399-pc base board */
+	ROC_RK3399_MEZZ_M2_POE,		/* roc-rk3399-Mezz M.2 PoE */
+};
+
 int board_early_init_f(void)
 {
 	struct udevice *bus, *dev;
 	int ret;
+
+	/* default board type */
+	gd->board_type = ROC_RK3399_PC;
 
 	ret = uclass_get_device_by_seq(UCLASS_I2C, BUS_NUM, &bus);
 	if (ret) {
@@ -56,8 +64,23 @@ int board_early_init_f(void)
 		return ret;
 	}
 
+	gd->board_type = ROC_RK3399_MEZZ_M2_POE;
+
 	return 0;
 }
+
+#ifdef CONFIG_SPL_LOAD_FIT
+int board_fit_config_name_match(const char *name)
+{
+	if (gd->board_type == ROC_RK3399_PC && !strcmp(name, "rk3399-roc-pc"))
+		return 0;
+
+	if (gd->board_type == ROC_RK3399_MEZZ_M2_POE && !strcmp(name, "rk3399-roc-pc-mezzanine"))
+		return 0;
+
+	return -EINVAL;
+}
+#endif
 
 #endif /* CONFIG_SPL_BUILD */
 
